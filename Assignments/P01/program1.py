21f68cf1e3e2b315272afbb791ed67a8fedb22ea
paigechampagne <paigechamp@gmail.com>
2022-02-28 15:47:56 -0600

    gjh

--- Assignments/P01/program1.py
+++ Assignments/P01/program1.py
@@ -1,7 +1,7 @@
 import json
 import random
 
-with open("P01\cities.json") as f:
+with open("Assignments\P01\cities.json") as f:
   data = json.load(f)
 
 def randColor():
@@ -31,15 +31,41 @@ def makePoint(city):
 
   return feature
 
+states = {}
+for item in data:
+    if not item["state"] in states:
+      states[item["state"]] = []
+    states[item["state"]].append(item)
+
+
+greatstate = {}
+
+for city in states:
+  for c in states[city["state"]]:
+    if not c["population"] in greatstate:
+      greatstate[c["population"]] = []
+      greatstate[c["population"]].append(c)
+    elif int(c["population"]) > int(greatstate[c["population"]]):
+      greatstate[c["population"]] = c["population"]
 
 
 
-
+# for city in states:
+#   if not states[city] in greatstate:
+#       greatstate[states[city]] = []
+#   elif states[city["population"]] > greatstate[states]:
+#       greatstate[states[city]] = states[city]
+  # print(f"{state} = {len(states[state])}") #prints amount of times the state appears in data
 
 points = []
-for info in data:
-  points.append(makePoint(info))
 
-with open("P01/new.geojson","w") as f:
+for stateInfo in data:
+  points.append(makePoint(stateInfo))
+
+# points = []
+# for info in data:
+#   points.append(makePoint(info))
+
+with open("Assignments\P01/new2.geojson","w") as f:
   json.dump(points,f,indent=4)
 
