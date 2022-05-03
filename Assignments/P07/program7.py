79ceb16b688fe7f840f97fee8d601995389fabeb
paigechampagne <paigechamp@gmail.com>
2022-05-03 06:43:13 -0500

    damn

---
+++ Assignments/P07/program7.py
@@ -0,0 +1,88 @@
+from quadTree import *
+import pygame
+from random import randint 
+
+import os
+  
+# Get the size
+# of the terminal
+size = os.get_terminal_size()
+
+x,y = size
+x *= 12
+y *= 12
+size = (x,y)
+print(x,y)
+
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('QuadTree')
+radius = 3
+
+def genPoints(bbox, x=100):
+    points  = []
+    for p in range(x):
+        x = randint(radius,bbox.w-radius)
+        y = randint(radius,bbox.h-radius)
+        points.append(Point(x,y))
+    return points
+
+def highlightPoints(foundPoints):
+    for p in foundPoints:
+        pygame.draw.circle(screen,(255,255,0),[p.x,p.y],radius+3)
+
+
+if __name__=='__main__':
+
+    print((size[0]//2,size[1]//2,size[0],size[1]))
+
+    bbox = Rect(size[0]//2,size[1]//2,size[0],size[1])
+    
+    qt = QuadTree(bbox)
+
+    points = genPoints(bbox,100)
+
+    nw = (size[0]//4,size[1]//4,size[0]//2,size[1]//2)
+
+    for p in points:
+        qt.insert(p);
+
+    foundPoints = []
+    
+    
+
+        
+    # creating a bool value which checks
+    # if game is running
+    running = True
+    
+    # Game loop
+    # keep game running till running is true
+    while running:
+        
+        screen.fill((255,255,255))
+        
+        # Check for event if user has pushed
+        # any event in queue
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                    # register click position
+                # r = (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], pygame.mouse.get_pos()[0]*100, pygame.mouse.get_pos()[1]*100)
+                pygame.draw.rect(screen,(0,0,0),(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], pygame.mouse.get_pos()[0]*100, pygame.mouse.get_pos()[1]*100), 3)
+                # qt.bbox = r
+                # qt.query(r,foundPoints)
+            # if event is of type quit then set
+            # running bool to false
+            if event.type == pygame.QUIT:
+                running = False
+
+        qt.draw(screen,radius)
+        highlightPoints(foundPoints)
+        # pygame.draw.rect(screen,(123,200,77),(0,0,nw[2],nw[3]),3)
+        # qt.query(Rect(size[0]//4,size[1]//4,size[0]//2,size[1]//2),foundPoints)
+        
+        # print(foundPoints)
+        
+
+        pygame.display.flip()
